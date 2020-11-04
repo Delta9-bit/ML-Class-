9fd467caebad8a78f642ccd60fa880983a9f03ec
Delta9-bit <lucas.aurouet@hotmail.fr>
2020-11-04 18:03:40 +0100

    update

---
+++ Projet.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import kaggle
+
+kaggle competitions download -c tpu-getting-started
